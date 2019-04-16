375f90c5c0060e8ceaca9fe4ca147296960a7993
Anmol Gautam <tarptaeya@gmail.com>
2019-04-17 01:10:19 +0530

    added templates and api/top-tweets route

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import jsonify
 from flask import render_template
 
 app = Flask(__name__)
@@ -6,6 +7,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', loggedIn=True)
+
+@app.route('/api/top-tweets', methods=['POST'])
+def top_tweets():
+    tweets = []
+    import random
+    for i in range(random.randint(2, 10)):
+        tweets.append({'title': random.randint(1, 100)})
+    fake_data = {'tweets': tweets}
+    return jsonify(fake_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
