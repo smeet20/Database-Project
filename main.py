ac210b58a801c35dc93c5ffd1df6dd977b4efcbd
Anmol Gautam <tarptaeya@gmail.com>
2019-04-24 01:01:46 +0530

    added auth && search

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ from flask import Flask
 from flask import jsonify
 from flask import render_template
 from flask import g
+from flask import request
+from flask import session
+from flask import redirect
+from flask import url_for
+
+from passlib.hash import sha256_crypt
+
 import sqlite3
 
 
 app = Flask(__name__)
+app.secret_key = 'I love rach**'
 
 import threading
 
@@ -26,8 +34,51 @@ def query_db(query, args=(), one=False):
     cur = get_db().execute(query, args)
     rv = cur.fetchall()
     cur.close()
-    
     return (rv[0] if rv else None) if one else rv
+
+def create_user(name, email, password):
+    pass_sh = sha256_crypt.encrypt(password)
+    cur = get_db()
+    query = cur.execute('insert into users (name, email, password) values (?, ?, ?)', (name, email, pass_sh))
+    cur = get_db().execute(query)
+ 
+def validate_user(email, password):
+    query = 'select * from users where email = ?'
+    record_pass = query_db(query, args=(email,), one=True)[3]
+    result = sha256_crypt.verify(password, record_pass)
+    return result
+
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form['email']
+    password = request.form['password']
+    res = validate_user(email, password)
+    if res:
+        session['email'] = email
+        return redirect(url_for('index'))
+ 
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.pop('email', None)
+    return redirect(url_for('index'))
+
+@app.route('/signup', methods = ['POST'])
+def signup():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not name: return
+    if not email: return
+    if not password: return
+    pass_sh = sha256_crypt.encrypt(password)
+    db = get_db()
+    try:
+        db.execute('insert into users (name, email, password) values (?, ?, ?)', (name, email, pass_sh))
+        db.commit()
+        session['email'] = email
+    except:
+        db.rollback()
+    return redirect(url_for('index'))
 
 @app.teardown_appcontext
 def close_connection(exception):
@@ -37,7 +88,33 @@ def close_connection(exception):
         
 @app.route('/')
 def index():
-    return render_template('index.html', loggedIn=True)
+    email = session.get('email')
+    loggedIn = True if email else False
+    return render_template('index.html', loggedIn=loggedIn, email=email)
+
+
+@app.route('/api/search')
+def search():
+    query = request.args['q']
+    cnt = request.args.get('cnt')
+    if not cnt:
+        cnt = 10
+    tweets = []
+    res = query_db("select * from tweets where text like '%{}%' order by time desc limit {}".format(query, cnt))
+    for tweet in res:
+        tweets.append({'text': tweet['text']})
+    data = {'tweets': tweets, 'type': 'Tweets like "{}"'.format(query)}
+    return jsonify(data)
+
+@app.route('/api/recent-tweets', methods=['POST'])
+def recent_tweets():
+    tweets = []
+    res = query_db('select * from tweets order by time desc limit 10')
+    for tweet in res:
+        tweets.append({'text': tweet['text']})
+    data = {'tweets': tweets, 'type': 'Recent Tweets'}
+    return jsonify(data)
+
 
 @app.route('/api/top-tweets', methods=['POST'])
 def top_tweets():
@@ -45,10 +122,10 @@ def top_tweets():
     res = query_db('select * from tweets LIMIT 5')
     for tweet in res:
         tweets.append({'text': tweet['text']})
-    data = {'tweets': tweets}
+    data = {'tweets': tweets, 'type': 'Top Tweets'}
     return jsonify(data)
 
 if __name__ == '__main__':
     threading.Thread(target=collect_data).start()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
 
