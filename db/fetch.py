f4d4125f2ccb35150aca9afc68f85647381a470d
Apoorv Ubu18 <apoorvagni@hotmail.com>
2019-04-24 14:28:27 +0530

    location

--- db/fetch.py
+++ db/fetch.py
@@ -16,7 +16,7 @@ def scrape(query):
     req = requests.get(url, headers={'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.128 Safari/537.36'})
     html_doc = req.text
     soup = BeautifulSoup(html_doc, 'html.parser')
-    
+
     # adding the event, we would not call scrape else.
     cur = Conn.cursor()
     cur.execute('insert into events (text) values (?)', (query,))
@@ -27,27 +27,21 @@ def scrape(query):
 
         # trying to get the location
         try:
-            temp = list(meta_data['data-permalink-path'])
-            ix = temp[1:].index('/')
-            user_href = temp[:ix+1]
-            url = 'https://www.twitter.com'+''.join(user_href)
-            req_user = requests.get(url, headers={'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.128 Safari/537.36'})
+            url_user = 'https://www.twitter.com/' + ''.join(list(meta_data['data-permalink-path'])).split('/')[1]
+            req_user = requests.get(url_user, headers={'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.128 Safari/537.36'})
             html_doc_user = req_user.text
             soup_user = BeautifulSoup(html_doc_user, 'html.parser')
             tt = soup_user.select('div[class="ProfileHeaderCard-location"]')
-            location = tt[0].a.string
-            if ',' in location:
-                ix2 = location.index(',')
-                location = location[ix2 + 1, :]
+            location = tt[0].a.string.split(',')[-1]
         except:
             pass # let go.
         user_name = meta_data['data-name']
         tweet_id = meta_data['data-tweet-id']
         parent_id = meta_data['data-conversation-id']
         timestamp = int(meta_data.select_one('._timestamp')['data-time-ms'])
-        
+
         text = str(meta_data.select_one('.js-tweet-text-container'))
-        
+
         try:
             image = meta_data.select_one('.AdaptiveMedia-container').select_one('img')['src']
             text += '<div class="TwitterImage" style="background-image: url({});"></div>'.format(image)
@@ -57,7 +51,7 @@ def scrape(query):
         likes = int(meta_data.select_one('.ProfileTweet-action--favorite > .ProfileTweet-actionCount')['data-tweet-stat-count'])
         replies = int(meta_data.select_one('.ProfileTweet-action--reply > .ProfileTweet-actionCount')['data-tweet-stat-count'])
         retweets = int(meta_data.select_one('.ProfileTweet-action--retweet > .ProfileTweet-actionCount')['data-tweet-stat-count'])
-                        
+          
         cur = Conn.cursor()
         try:
             cur.execute('insert into author (id, name) values (?, ?)', (user_id, user_name))
@@ -67,7 +61,7 @@ def scrape(query):
         except sqlite3.Error as e:
             print(e)
             Conn.rollback()
-            
+           
 def collect_data():
     global Conn
     Conn = sqlite3.connect('./database.db')
@@ -75,7 +69,7 @@ def collect_data():
         for query in Queries:
             scrape(query)
             time.sleep(120)
-        
+       
 if __name__ == '__main__':
     collect_data()
     scrape('hello')
